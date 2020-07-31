0fc56b9b541006ef02e0d5e4a5b6b3417fa10b8c
hoangdh143 <huuhoang.dao@gmail.com>
2020-08-01 01:21:46 +0700

    Update as tuts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCvzuQ3Q8Uh8XiupsTdY3apBfdor0QdAac',
+    authDomain: 'pizza-planet-90f53.firebaseapp.com',
+    databaseURL: 'https://pizza-planet-90f53.firebaseio.com',
+    projectId: 'pizza-planet-90f53',
+    storageBucket: 'pizza-planet-90f53.appspot.com',
+    messagingSenderId: '425874707839',
+    appId: '1:425874707839:web:c7bbc3b328aec8ab0a364a'
+  }
 };
